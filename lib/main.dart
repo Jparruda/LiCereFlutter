49ce8e557c6a27ea8f8cab1db191f31da86b5ebf
João Pedro Souza Arruda <joao.arruda.jope16@gmail.com>
2024-04-06 12:51:27 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Licere',
+      theme: ThemeData(
+        useMaterial3: true,
+        textTheme: GoogleFonts.lobsterTwoTextTheme(),
+      ),
+      home: const MyHomePage(title: 'LiCere'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    final double fontSize = screenSize.width * 0.12; // 5% da largura da tela
+    return Scaffold(
+      backgroundColor: const Color(0xFF808DFF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              "LiCere",
+              style: GoogleFonts.lobsterTwo(
+                textStyle: TextStyle(
+                  fontSize: fontSize,
+                  color: const Color(0xFF09E762),
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  shadows: const [
+                    Shadow(
+                      blurRadius: 14.2,
+                      color: Color.fromARGB(78, 0, 0, 0),
+                      offset: Offset(-6.0, 6.0),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF09E762)),
+                foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 47, 156, 91)),
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                ),
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    side: const BorderSide(color: Colors.white), // Adiciona a borda
+                  ),
+                ),
+                elevation: MaterialStateProperty.all<double>(30.0), // Adiciona a sombra
+              ),
+              child: Text(
+                "JOGAR",
+                style: GoogleFonts.montserrat(
+                  textStyle: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
